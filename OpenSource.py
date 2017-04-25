9eb2789d2b048f33996b7cdedc862ad23d8c3ac5
BeastKing0 <maxxmc4@gmail.com>
2017-04-25 15:44:13 -0400

    privated ID Token

--- OpenSource.py
+++ OpenSource.py
@@ -16,4 +16,4 @@ async def on_message(message):
     if message.content.startswith('-bot'):
         await client.send_message(message.channel, 'Open Source collective v.0 (patch #1.0.0')
 
-client.run('MzA2NDkzOTMzNjU0MTE0MzE2.C-EkSw.S4A6o3Eeor8OFpcybUSOphtVYL8')
+client.run('hidden token')

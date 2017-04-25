dbd206cc1d80247b78955e0e3d14a523ca1417bc
BeastKing0 <maxxmc4@gmail.com>
2017-04-25 15:54:39 -0400

    Added -source command.
    Displays https://github.com/BeastKing0/Coders-Delight-Bot source code.

--- OpenSource.py
+++ OpenSource.py
@@ -15,5 +15,7 @@ async def on_message(message):
     await client.change_presence(game=discord.Game(name='Anyones Bot'))
     if message.content.startswith('-bot'):
         await client.send_message(message.channel, 'Open Source collective v.0 (patch #1.0.0')
+    if message.content.startswith('-source'):
+        await client.send_message(message.channel, 'Source Code: https://github.com/BeastKing0/Coders-Delight-Bot')
 
 client.run('hidden token')

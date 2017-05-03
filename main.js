fd2c02f7717f7684a7b3641e963652a4b68323c2
Wingysam <wingysam@gmail.com>
2017-05-03 07:38:06 -0400

    Create API

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ esversion: 6
 */
 const Discord = require('discord.js');
 const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const json = require('express-json');
+var app = express();
 
 // bot logging, useful for debugging if the windows closes
 fs.writeFile('log', '', function (err) {});
@@ -18,6 +22,8 @@ var client = new Discord.Client();
 // Load configuration
 var config = require(__dirname + '/config.json');
 
+var newMessages = [];
+
 client.on('ready', () => {
   logger.log('Logged in as');
   logger.log(client.user.username);
@@ -31,9 +37,27 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    newMessages.push({content: message.content, author: message.author.id, channel: message.channel.id});
     if (message.content.startsWith(config.prefix + 'bot')) message.channel.send('Open Source collective v.0 (patch #1.0.0)');
     if (message.content.startsWith(config.prefix + 'source')) message.channel.send('Source Code: https://github.com/BeastKing0/Coders-Delight-Bot');
   }
 );
 
 client.login(config.token);
+
+app.use(bodyParser.json());
+
+app.use(json());
+
+app.post('/send', function (req, res) {
+  client.channels.get(req.body.channelid).send(req.body.message);
+  res.end('Sent');
+});
+
+app.get('/check', function (req, res) {
+  res.end(JSON.stringify(newMessages));
+});
+
+app.listen(3000, function() {
+  logger.log('Express started');
+});
